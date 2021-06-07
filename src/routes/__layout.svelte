d51055b9f36ce04705fc36418f311ab0b589e0b2
moritzebeling <git@moritzebeling.com>
2021-06-07 21:14:17 +0200

    basic page setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+    <a sveltekit:prefetch href="/">Home</a>
+    <a sveltekit:prefetch href="/public">Public</a>
+    <a sveltekit:prefetch href="/private">Private</a>
+</nav>
+
+<slot />
---
+++ src/routes/private/index.svelte
@@ -0,0 +1,2 @@
+<h1>Private Page</h1>
+<p>Please log in.</p>
