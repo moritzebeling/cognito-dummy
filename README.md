1a99ec5f252cc367a90f3a7d4bd9bfee099e0307
moritzebeling <git@moritzebeling.com>
2021-06-07 23:39:31 +0200

    ERROR try to config amplify

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This is a [Svelte Kit](https://kit.svelte.dev) app to test run [AWS Cognito](https://aws.amazon.com/de/cognito/).
 
+Links:
+- Svelte Realworld App https://github.com/sveltejs/realworld
+- Getting started with AWS Cognito https://docs.amplify.aws/lib/auth/getting-started/q/platform/js
+- Sign in & out https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js
+- JS SDK https://github.com/aws-amplify/amplify-js/tree/master/packages/amazon-cognito-identity-js
+- Auth Class https://aws-amplify.github.io/amplify-js/api/classes/authclass.html
+
 ## Install, Dev, Deploy
 ```
 npm install
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,41 @@
 <script>
 
+    import Amplify, { Auth } from 'aws-amplify';
+
+    Amplify.configure({
+        Auth: {
+
+            // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+            identityPoolId: 'eu-central-1:be559466-d17f-4f5f-85e6-b867927af67e',
+
+            // REQUIRED - Amazon Cognito Region
+            region: 'eu-central-1',
+
+            // OPTIONAL - Amazon Cognito User Pool ID
+            userPoolId: 'eu-central-1_InrFTmbei',
+
+            // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+            userPoolWebClientId: '2mmpk4rria1jdsgoeg9l73csnk',
+
+            // OPTIONAL - Configuration for cookie storage
+            // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+            cookieStorage: {
+                // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+                domain: '.yourdomain.com',
+                // OPTIONAL - Cookie path
+                path: '/',
+                // OPTIONAL - Cookie expiration in days
+                expires: 365,
+                // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+                sameSite: "strict",
+                // OPTIONAL - Cookie secure flag
+                // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+                secure: true
+            },
+
+        }
+    });
+
     import Navigation from '$lib/Navigation.svelte';
 
 </script>
